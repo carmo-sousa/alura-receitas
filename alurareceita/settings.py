61a28919159320ed05bbbde46bcd7b20451454b6
Romulo Sousa <carmosousa20@outlook.com>
2022-10-13 18:36:04 -0400

    Adcionando dominio as configuracoes

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-jlgkj_qcb=h#8ho*k(0d^3l!0ygl-lj&6k(^a75ftf4qtfkr)j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["http://ec2-54-232-78-4.sa-east-1.compute.amazonaws.com/"]
 
 
 # Application definition
