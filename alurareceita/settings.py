a4bae7bd13bc6f2d4f7947e6dd9bc9e3a5881f1c
Romulo Sousa <carmosousa20@outlook.com>
2022-10-14 22:10:34 -0400

    Criando o modela da classe

--- alurareceita/settings.py
+++ alurareceita/settings.py
@@ -124,11 +124,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "static"
-STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "alurareceita/static",
-]
+# STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "https://metatron-assets.s3.sa-east-1.amazonaws.com/static/"
+# STATICFILES_DIRS = [
+#     BASE_DIR / "alurareceita/static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
